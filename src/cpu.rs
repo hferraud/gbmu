5bdb8f1e5545753a7fe55a29d4c9edf695b99e77
mururoahh <ferraudhugo@gmail.com>
2024-07-02 17:43:06 +0200

    refactor: remove debug log

--- src/cpu.rs
+++ src/cpu.rs
@@ -28,47 +28,20 @@ impl CPU {
     }
 
     pub fn run(&mut self, mmu: &mut MMU) -> Result<(), io::Error> {
-        let mut breakpoint: bool = false;
         loop {
-            if breakpoint {
-                println!("New instruction:");
-                println!("PC: {:#06x}", self.registers.pc);
-                println!("Opcode");
-            }
             let word = self.fetch_next_word(mmu)?;
-            if breakpoint {
-                println!("Instruction:");
-            }
             instructions::execute(word, self, mmu)?;
-            if breakpoint {
-                println!("{:#x?}", self.registers);
-                println!();
-            }
-            if self.registers.pc == 0x29a {
-                breakpoint = true;
-            }
-            if breakpoint {
-                let mut input = String::new();
-                io::stdin().read_line(&mut input)
-                    .expect("Erreur de lecture");
-                if input.trim() == "q" {
-                    break;
-                }
-            }
         }
-        Ok(())
     }
 
     pub fn fetch_next_word(&mut self, mmu: &mut MMU) -> Result<u8, io::Error> {
         let word = mmu.get_word(self.registers.pc as usize)?;
-        println!("word: {:#04x} = {:#010b}", word, word);
         self.registers.pc += mem::size_of::<Word>() as DWord;
         Ok(word)
     }
 
     pub fn fetch_next_dword(&mut self, mmu: &mut MMU) -> Result<u16, io::Error> {
         let dword = mmu.get_dword(self.registers.pc as usize)?;
-        println!("dword: {:#06x} = {:#018b}", dword, dword);
         self.registers.pc += mem::size_of::<DWord>() as DWord;
         Ok(dword)
     }
